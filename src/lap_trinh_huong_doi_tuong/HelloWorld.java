17794eee41bfa3df9c540f2ff99c240efdaff334
Phi Son <sonpq@mail.wgentech.com>
2024-07-14 14:18:14 +0700

    test

--- src/lap_trinh_huong_doi_tuong/HelloWorld.java
+++ src/lap_trinh_huong_doi_tuong/HelloWorld.java
@@ -1,11 +1,15 @@
 package lap_trinh_huong_doi_tuong;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world with java ");
-		System.out.println("Hello world with java ");
-		System.out.println("Hello world with java ");
-		System.out.println("Hello world with java ");
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Your name is: ");
+		String name = scanner.nextLine();
+		System.out.println("Your age is: ");
+		int age = scanner.nextInt();
+		System.out.println("My name is " + name + ", age = " + age);
+		scanner.close();
 	}
 }
